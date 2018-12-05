d101541789d84555a2e3bb5e96b09f37a57044de
Tom Ceuppens <t-c@live.be>
2018-12-05 14:18:40 +0100

    Meta data + tests

--- src/chatbot/validation.ts
+++ src/chatbot/validation.ts
@@ -2,6 +2,7 @@ import * as Joi from 'joi';
 
 const messagevalidation = {
     message: Joi.string().required(),
+    meta: Joi.object(),
     send: Joi.boolean(),
     session_id: Joi.string().required(),
     type: Joi.string(),
